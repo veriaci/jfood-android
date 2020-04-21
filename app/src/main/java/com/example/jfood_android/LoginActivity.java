b19852c8c8b2a174caa7587bcac23d5ba78abbce
Hanif Imam <decadex99@gmail.com>
2020-04-21 19:41:52 +0700

    TP Module 09

--- app/src/main/java/com/example/jfood_android/LoginActivity.java
+++ app/src/main/java/com/example/jfood_android/LoginActivity.java
@@ -10,6 +10,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
@@ -28,11 +35,31 @@ public class LoginActivity extends AppCompatActivity {
                     String email = etEmail.getText().toString();
                     String password = etPassword.getText().toString();
 
+                    Response.Listener<String> responseListener = new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jsonObject = new JSONObject(response);
+                                if (jsonObject != null){
+                                    Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_LONG).show();
+                                }
+                            } catch (JSONException e){
+                                Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    };
+
+                    LoginRequest loginRequest = new LoginRequest(email, password, responseListener);
+                    RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                    queue.add(loginRequest);
+
+                    /*
                     if(email.equals("test@test.com") && password.equals("test")){
                         Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
                     } else {
                         Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
                     }
+                     */
             }
         });
 
--- app/src/main/java/com/example/jfood_android/RegisterActivity.java
+++ app/src/main/java/com/example/jfood_android/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.jfood_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,6 +28,7 @@ public class RegisterActivity extends AppCompatActivity {
         final EditText etEmail = findViewById(R.id.etEmail);
         final EditText etPassword = findViewById(R.id.etPassword);
         Button btnRegister = findViewById(R.id.btnRegister);
+        TextView tvLogin = findViewById(R.id.tvLogin);
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,6 +57,13 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
